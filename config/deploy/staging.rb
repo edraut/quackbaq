3e76defc24e7cf2aa6cfbef845c66bf8a9e2fcfa
Eric Draut <edraut@gmail.com>
2010-10-28 11:39:40 -0600

    added deploy stage config

---
+++ config/deploy/staging.rb
@@ -0,0 +1,10 @@
+role :app, "184.106.135.19"
+role :web, "184.106.135.19"
+role :db,  "184.106.133.196", :primary => true
+
+set :db_user, "quackbaq"
+set :db_pass, "qu4ckb4q"
+
+set :branch, "staging"
+
+set :rails_env, "production"
